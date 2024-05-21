7e1e73742afcd333434ded4fd89553a65bd78bab
sinsj5858 <goddnjswns58@gmail.com>
2024-05-21 17:53:07 +0900

    add : requestMapping /api 추가

--- src/main/java/com/fc/testcicd/controller/testController.java
+++ src/main/java/com/fc/testcicd/controller/testController.java
@@ -3,9 +3,11 @@ package com.fc.testcicd.controller;
 import com.fc.testcicd.service.TestService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class testController {
     private final TestService testService;
